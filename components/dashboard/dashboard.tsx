546358fa7497f6ec226087eb4e8a3bb10205bb71
Arjun Patel <55220892+arjunpatel01@users.noreply.github.com>
2023-04-05 23:47:40 -0500

    display cards (err: they cut off at the bottom)

--- components/dashboard/dashboard.tsx
+++ components/dashboard/dashboard.tsx
@@ -1,33 +1,30 @@
-import { Card, Grid, List, Paper } from "@mui/material";
+import { Box, Card, Grid, List, Paper } from "@mui/material";
 import React from "react";
 import style from "./dashboard.module.scss"
+
+const CARDS = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
 
 export default function Dashboard() {
     return (
         <React.Fragment>
             <Grid container spacing={0} width={"100%"} overflow={"hidden"} sx={{overflow: "hidden"}}>
-                <Grid xs={3} sx={{paddingTop: "20rem", width: "100%"}}>
+                <Grid item xs={3} sx={{paddingTop: "10%", width: "100%", height: "auto"}}>
                     <Card>
                         xs = 3
                     </Card>
                 </Grid>
-                <Grid xs={6} sx={{width: "100%"}}>
-                    <List className={style.example} style={{maxHeight: "85vh", overflow: 'auto',}}>
-                        <Card sx={{height: "300px", marginTop: "10px"}}>
-                            xs = 6
-                        </Card>
-                        <Card sx={{height: "300px", marginTop: "10px"}}>
-                            xs = 6
-                        </Card>
-                        <Card sx={{height: "300px", marginTop: "10px"}}>
-                            xs = 6
-                        </Card>
-                        <Card sx={{height: "300px", marginTop: "10px"}}>
-                            xs = 6
-                        </Card>
+                <Grid xs={6} sx={{width: "100%", paddingRight: "5%", paddingLeft: "5%",}}>
+                    <List className={style.invisiScroll} style={{maxHeight: "20%", overflow: 'auto', justifyContent: "center", width: "100%", borderRadius: 3}}>
+                        {CARDS.map((card, index) => (
+                            <div style={{paddingTop: "10px"}}>
+                                <Card key={index} sx={{height: "300px",}}>
+                                    {card}
+                                </Card>
+                            </div>
+                        ))}
                     </List>
                 </Grid>
-                <Grid xs={3} sx={{paddingTop: "10rem", width: "100%"}}>
+                <Grid xs={3} sx={{paddingTop: "10%", width: "100%", height: "auto"}}>
                     <Card>
                         xs = 3
                     </Card>
