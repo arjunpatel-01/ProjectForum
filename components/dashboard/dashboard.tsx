77e0359a0536c1149bc7046583a30ad8a278cd31
Arjun Patel <55220892+arjunpatel01@users.noreply.github.com>
2023-04-05 20:32:56 -0500

    basic grid pattern with hidden-scrollable center

--- components/dashboard/dashboard.tsx
+++ components/dashboard/dashboard.tsx
@@ -1,5 +1,39 @@
+import { Card, Grid, List, Paper } from "@mui/material";
+import React from "react";
+import style from "./dashboard.module.scss"
+
 export default function Dashboard() {
     return (
-        <p>Hello From Dashboard</p>
+        <React.Fragment>
+            <Grid container spacing={0} width={"100%"} overflow={"hidden"} sx={{overflow: "hidden"}}>
+                <Grid xs={3} sx={{paddingTop: "20rem", width: "100%"}}>
+                    <Card>
+                        xs = 3
+                    </Card>
+                </Grid>
+                <Grid xs={6} sx={{width: "100%"}}>
+                    <List className={style.example} style={{maxHeight: "85vh", overflow: 'auto',}}>
+                        <Card sx={{height: "300px", marginTop: "10px"}}>
+                            xs = 6
+                        </Card>
+                        <Card sx={{height: "300px", marginTop: "10px"}}>
+                            xs = 6
+                        </Card>
+                        <Card sx={{height: "300px", marginTop: "10px"}}>
+                            xs = 6
+                        </Card>
+                        <Card sx={{height: "300px", marginTop: "10px"}}>
+                            xs = 6
+                        </Card>
+                    </List>
+                </Grid>
+                <Grid xs={3} sx={{paddingTop: "10rem", width: "100%"}}>
+                    <Card>
+                        xs = 3
+                    </Card>
+                </Grid>
+            </Grid>
+        </React.Fragment>
+        
     )
 }
