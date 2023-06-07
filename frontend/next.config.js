bf7af014c2fea34dea26c118983f5a7da1567366
Arjun Patel <55220892+arjunpatel01@users.noreply.github.com>
2023-06-06 23:01:19 -0500

    switch back to local

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,19 +5,14 @@ const nextConfig = {
     return [
       {
         source: '/api/:slug*',
-        destination: 'http://ec2-3-80-132-185.compute-1.amazonaws.com/:slug*'
+        // destination: 'http://ec2-3-80-132-185.compute-1.amazonaws.com/:slug*'
+        destination: 'http://localhost:8080/:slug*'
       },
     ]
   }
 }
 
-// export async function rewrites() {
-//   return [
-//     {
-//       source: '/api/:slug*',
-//       destination: 'http://localhost:8080/:slug*'
-//     },
-//   ]
-// }
+//comment for package.json since json doesnt like comments
+//"proxy": "http://localhost:8080",
 
 module.exports = nextConfig
